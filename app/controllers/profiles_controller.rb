a3479db619e9069c4e408e580a8c8a78ee05aa58
Bejan Maljai <bmaljai@gmail.com>
2016-02-06 21:26:43 -0800

    added profile creation for only alumni

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -44,7 +44,7 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile).permit(:firstname, :middlename, :lastname)
+    params.require(:profile).permit(:firstname, :middlename, :lastname, :user_id)
   end
 
 
