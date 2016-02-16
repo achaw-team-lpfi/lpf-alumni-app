0a8c89fe1ab3bc8fd5b0e39a65bba89ebe9ba42e
Bejan Maljai <bmaljai@gmail.com>
2016-02-15 18:12:02 -0800

    added form states

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 100.times do
-  Profile.create(firstname: Faker::Name.first_name, middlename: Faker::Name.first_name, lastname: Faker::Name.last_name)
+  Profile.create(
+    firstname: Faker::Name.first_name, 
+    middlename: Faker::Name.first_name, 
+    lastname: Faker::Name.last_name,
+    birthdate: Faker::Date.backward(5000),
+    address1: Faker::Address.street_address,
+    address2: Faker::Address.secondary_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip: Faker::Address.zip,
+    email: Faker::Internet.email,
+    homephone: Faker::PhoneNumber.cell_phone,
+    workphone: Faker::PhoneNumber.cell_phone,
+    cellphone: Faker::PhoneNumber.cell_phone,
+    current_city: Faker::Address.city,
+    current_state: Faker::Address.state_abbr,
+    )
 end
