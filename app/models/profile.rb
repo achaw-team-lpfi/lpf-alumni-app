44eb242056e04e96f2a63b18ca4ad06325ecf752
Bejan Maljai <bmaljai@gmail.com>
2016-02-15 17:38:43 -0800

    updated show fields and display

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,16 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
 
+  def name_format
+    return "#{lastname}, #{firstname} #{middlename if middlename}"
+  end
+
   def fullname
     return "#{firstname} #{middlename if middlename} #{lastname}"
   end
+
+  def change_me
+    return "FILLER TEXT, ADD FIELD"
+  end
+
 end
