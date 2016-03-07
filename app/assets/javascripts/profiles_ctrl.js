4d1d20b35e6c9d30b02328b0ce5c157eed4dce07
allencwwong <allencwwong@gmail.com>
2016-03-06 18:12:06 -0800

    update condition for user vs admin view on search

--- app/assets/javascripts/profiles_ctrl.js
+++ app/assets/javascripts/profiles_ctrl.js
@@ -5,7 +5,7 @@
     
     $scope.message = "Angular Controller Added!";
 
-      // pull data from profile.json 
+    // pull data from profile.json 
 
     $http.get("/api/v1/profiles.json")
       .success(function(data, status, headers, config){
@@ -18,8 +18,16 @@
     $scope.searchTerm = ''; // ng-model for search term
 
     //check for global var searchTerm from other pages if someone use search from nav bar it will pass params into angular filter
+    $scope.viewable = function viewable(){
+      if (current_user === "admin"){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    //current_user
 
-    console.log(current_user);
 
     // search function to display only id matches search term (additional add-on if time permit // change from filter to search by dropdown)
     $scope.search = function(){
